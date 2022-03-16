19f0cbbdc6f7013d59968c4b323d0e229695df83
toxi42 <79251578+toxi42@users.noreply.github.com>
2022-03-16 04:42:16 +0300

    1

--- README.md
+++ README.md
@@ -4,7 +4,13 @@ tools and scripts for linux systems
 ### change ssh port
 
 ```sh
-echo 'port 34777' >> /etc/ssh/sshd_config
+sudo sed -i 's/#Port 22/Port 34777/' /etc/ssh/sshd_config
 ufw allow 34777/tcp
 systemctl restart sshd
+```
+### ssh root login enable
+
+```sh
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+echo -e "linuxpassword\nlinuxpassword" | passwd root
 ```
--- softether-ubuntu/softether-install.sh
+++ softether-ubuntu/softether-install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 timedatectl set-timezone Europe/Moscow
-apt install -y jq wget mc pkg-config cmake libncurses-dev libssl-dev zlib1g-dev libsodium-dev libpthread-stubs0-dev libncurses5-dev libreadline6-dev
-systemctl stop vpnserver.service
+ufw allow 443,1194,5555/tcp && ufw allow 500,1701,4500/udp
+sudo apt update
+sudo apt install -y jq wget mc build-essential pkg-config cmake libncurses-dev libssl-dev zlib1g-dev libsodium-dev libpthread-stubs0-dev libncurses5-dev libreadline6-dev
+sudo systemctl stop vpnserver.service
+
 rm -rf $HOME/vpnserver
 cd; \
 SEversion=`wget -qO- https://api.github.com/repos/SoftEtherVPN/SoftEtherVPN_Stable/releases/latest | jq -r '.assets | .[].browser_download_url' | grep linux-x64-64bit | grep vpnserver`; \
@@ -10,23 +13,56 @@ tar xvf softethervpn.tar.gz; \
 cd vpnserver; \
 ./.install.sh
 
-mv -b vpnserver vpncmd hamcore.se2 /usr/bin/
+mkdir /opt/vpnserver/
+mv -b vpnserver vpncmd hamcore.se2 /opt/vpnserver/
 
-sudo tee /lib/systemd/system/vpnserver.service > /dev/null <<EOF
-[Unit]
-Description=SoftEther VPN Server
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/usr/bin/vpnserver start
-ExecStop=/usr/bin/vpnserver stop
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload; \
-systemctl enable vpnserver; \
-systemctl restart vpnserver; \
-journalctl -u vpnserver -f
+PS3='\nFirst connect to your SoftEther server with Manager and create local vpn bridge with New Tap Device "soft"! If unsure or are using SecureNAT, select No.\n\n'
+options=("Yes" "No" "Quit")
+select opt in "${options[@]}"
+do
+case $opt in
+"Yes")
+        printf "\nDNSMASQ setup.\n\n"
+        apt install -y dnsmasq
+        cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
+        wget -qO /etc/dnsmasq.conf https://raw.githubusercontent.com/starnodes/linux-tools/main/softether-ubuntu/dnsmarq > /dev/null 2>&1
+        printf "\nSystem daemon setup. Registering changes...\n\n"
+        wget -qO /etc/init.d/vpnserver https://raw.githubusercontent.com/starnodes/linux-tools/main/softether-ubuntu/service-bridge > /dev/null 2>&1
+        chmod 755 /etc/init.d/vpnserver
+        update-rc.d vpnserver defaults > /dev/null 2>&1
+        printf "\nSoftEther VPN Server should now start as a system service from now on.\n\n"
+        systemctl start vpnserver
+        systemctl restart dnsmasq
+        systemctl enable dnsmasq
+        printf "\nCleaning up...\n\n"
+        systemctl is-active --quiet vpnserver && echo "Service vpnserver is running.\n\n"
+        printf "\nDo not forget enable UFW!!!\n\n"
+        printf "\nIMPORTANT !!! If you haven't created a local bridge yet with New Tap Device "soft" by using the SoftEther VPN Server Manager then DO IT. It is important that after you add the local bridge, you restart both dnsmasq and the vpnserver!\n\n"
+	      sleep 5s
+        break
+            ;;
+        "No")
+	       printf "\nSystem daemon setup. Registering changes...\n\n"
+        wget -qO /etc/init.d/vpnserver https://raw.githubusercontent.com/starnodes/linux-tools/main/softether-ubuntu/service-SN > /dev/null 2>&1
+        chmod 755 /etc/init.d/vpnserver
+        update-rc.d vpnserver defaults > /dev/null 2>&1
+        printf "\nSoftEther VPN Server should now start as a system service from now on.\n\n"
+        systemctl start vpnserver
+        cp /opt/vpnserver/vpn_server.config /opt/vpnserver/vpn_server.config.bak
+        sed -i '1,50s/bool Disabled false/bool Disabled true/' /opt/vpnserver/vpn_server.config
+        sed -i 's/bool DisableNatTraversal false/bool DisableNatTraversal true/' /opt/vpnserver/vpn_server.config
+        sed -i 's/bool DisableUdpAcceleration false/bool DisableUdpAcceleration true/' /opt/vpnserver/vpn_server.config
+        systemctl restart vpnserver
+        printf "\nCleaning up...\n\n"
+        systemctl is-active --quiet vpnserver && echo "Service vpnserver is running."
+	      printf "\nDo not forget enable UFW!!!\n\n"
+        printf "\n!!! IMPORTANT !!!\n\nPlese configure the server with SecureNAT, use the SoftEther VPN Server Manager!!!\n\n"
+        break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+esac
