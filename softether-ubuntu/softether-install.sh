b9c2251ac597729820aadcf051a57788dd54cb84
Anton Tugunov <79251578+starnodes@users.noreply.github.com>
2023-03-18 03:01:39 +0300

    Update softether-install.sh

--- softether-ubuntu/softether-install.sh
+++ softether-ubuntu/softether-install.sh
@@ -93,6 +93,7 @@ sudo iptables -t mangle -F
 sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A FORWARD -i tap_soft -o $ETHWAN -j ACCEPT
 sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A INPUT -p tcp -m multiport --dports 22,34777 -j ACCEPT
 sudo iptables --policy INPUT DROP
 sudo iptables --policy OUTPUT ACCEPT
