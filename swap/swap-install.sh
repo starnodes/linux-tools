76b2fd56383b7052bcb4f42349775e8d4b1966e5
Starnodes <79251578+starnodes@users.noreply.github.com>
2025-02-20 17:15:00 +0300

    Update swap-install.sh

--- swap/swap-install.sh
+++ swap/swap-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo swapoff -a
 rm "/swapfile"
-sudo fallocate -l 4G /swapfile
+sudo fallocate -l 8G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 swapon --show
-cp /etc/fstab /etc/fstab.back
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+#cp /etc/fstab /etc/fstab.back
+#echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
